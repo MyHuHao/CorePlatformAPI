c81f05ad1c17b47f2232405c0bcc1451ac585693
Mylinaa <hu0000604@outlook.com>
2025-05-05 13:29:09 +0800

    第二次提交

--- Infrastructure/Data/DapperExtensions.cs
+++ Infrastructure/Data/DapperExtensions.cs
@@ -1,6 +1,35 @@
+using Dapper;
+using System.Data;
+
 namespace Infrastructure.Data;
 
-public class DapperExtensions
+public static class DapperExtensions
 {
+    public static async Task<IEnumerable<T>> QueryAsync<T>(this IDbConnectionFactory factory, 
+        string databaseName, string sql, object? param = null)
+    {
+        using IDbConnection connection = factory.CreateConnection(databaseName);
+        return await connection.QueryAsync<T>(sql, param);
+    }
     
+    public static async Task<T> QueryFirstOrDefaultAsync<T>(this IDbConnectionFactory factory,
+        string databaseName, string sql, object? param = null)
+    {
+        using IDbConnection connection = factory.CreateConnection(databaseName);
+        return await connection.QueryFirstOrDefaultAsync<T>(sql, param) ?? throw new InvalidOperationException();
+    }
+
+    public static async Task<int> ExecuteAsync(this IDbConnectionFactory factory,
+        string databaseName, string sql, object? param = null)
+    {
+        using IDbConnection connection = factory.CreateConnection(databaseName);
+        return await connection.ExecuteAsync(sql, param);
+    }
+
+    public static async Task<T> ExecuteScalarAsync<T>(this IDbConnectionFactory factory,
+        string databaseName, string sql, object? param = null)
+    {
+        using IDbConnection connection = factory.CreateConnection(databaseName);
+        return await connection.ExecuteScalarAsync<T>(sql, param) ?? throw new InvalidOperationException();
+    }
 }
--- Infrastructure/Data/DbConnectionFactory.cs
+++ Infrastructure/Data/DbConnectionFactory.cs
@@ -1,6 +1,28 @@
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
 namespace Infrastructure.Data;
 
-public class DbConnectionFactory
+public interface IDbConnectionFactory
 {
-    
+    IDbConnection CreateConnection(string databaseName);
+}
+
+public class DbConnectionFactory(IConfiguration configuration) : IDbConnectionFactory
+{
+    private readonly string _connectionString = configuration.GetConnectionString("MasterDB")
+                                                ?? throw new InvalidOperationException();
+
+
+    public IDbConnection CreateConnection(string databaseName)
+    {
+        MySqlConnectionStringBuilder builder = new(_connectionString)
+        {
+            Database = databaseName
+        };
+        MySqlConnection connection = new(builder.ConnectionString);
+        connection.Open();
+        return connection;
+    }
 }
