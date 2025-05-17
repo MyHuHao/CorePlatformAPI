851c7203a3f54cbccdd0612fe2a75b9a0c37921d
MyHuHao <huhao660921@outlook.com>
2025-05-17 12:00:49 +0800

    提交代码

---
+++ Core/Helpers/ExcelHelper.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace Core.Helpers;
+
+public static class ExcelHelper
+{
+    public static MemoryStream Export<T>(List<T> data, List<string> names, List<Type> types,
+        string? sheetName = "Sheet1",
+        ICellStyle? cellStyle = null)
+    {
+        XSSFWorkbook workbook = new();
+        var sheet = workbook.CreateSheet(sheetName);
+        var headerRow = sheet.CreateRow(0);
+        if (cellStyle == null)
+        {
+            cellStyle = workbook.CreateCellStyle();
+            // 居中对齐
+            cellStyle.Alignment = HorizontalAlignment.Center;
+            cellStyle.VerticalAlignment = VerticalAlignment.Center;
+            // 四边加框
+            cellStyle.BorderTop = BorderStyle.Thin;
+            cellStyle.BorderBottom = BorderStyle.Thin;
+            cellStyle.BorderLeft = BorderStyle.Thin;
+            cellStyle.BorderRight = BorderStyle.Thin;
+            // 填满綠色
+            cellStyle.FillForegroundColor = HSSFColor.LightCornflowerBlue.Index;
+            cellStyle.FillPattern = FillPattern.SolidForeground;
+            // 粗体字形
+            var font = workbook.CreateFont();
+            font.IsBold = true;
+            cellStyle.SetFont(font);
+        }
+
+        var type = typeof(T);
+        var properties = type.GetProperties();
+        for (var i = 0; i < names.Count; i++)
+        {
+            var headerCell = headerRow.CreateCell(i);
+            headerCell.SetCellValue(names[i]);
+            headerRow.GetCell(i).CellStyle = cellStyle;
+        }
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            var dataRow = sheet.CreateRow(i + 1);
+            var item = data[i];
+            for (var j = 0; j < types.Count; j++)
+            {
+                var property = properties[j];
+                var value = property.GetValue(item);
+                var dataCell = dataRow.CreateCell(j);
+                if (value != null)
+                {
+                    if (types[j] == typeof(string))
+                        dataCell.SetCellValue(value.ToString());
+                    else if (types[j] == typeof(int) || types[j] == typeof(float) || types[j] == typeof(double) ||
+                             types[j] == typeof(decimal))
+                        //这样的话 单元格类型就不是文本类型
+                        dataCell.SetCellValue(double.Parse(value.ToString()!));
+                    else if (types[j] == typeof(DateTime))
+                        dataCell.SetCellValue(((DateTime)value).ToString("yyyy-MM-dd"));
+                    else if (types[j] == typeof(bool))
+                        dataCell.SetCellValue((bool)value ? "是" : "否");
+                    else
+                        dataCell.SetCellValue(value.ToString());
+                }
+            }
+        }
+
+        var headerAverageWidth = 0;
+        for (var i = 0; i < headerRow.LastCellNum; i++)
+        {
+            var cell = headerRow.GetCell(i);
+            if (cell == null) continue;
+            var cellWidth = Encoding.UTF8.GetBytes(cell.ToString()!).Length;
+            if (cellWidth > headerAverageWidth) headerAverageWidth = cellWidth;
+        }
+
+        var maxLength = new int[sheet.GetRow(0).LastCellNum + 1];
+        for (var rowIndex = sheet.FirstRowNum + 1; rowIndex <= sheet.LastRowNum; rowIndex++) // 跳过表头行  
+        {
+            var row = sheet.GetRow(rowIndex);
+            if (row == null) continue;
+            for (var cellIndex = 0; cellIndex < row.LastCellNum; cellIndex++)
+            {
+                var cell = row.GetCell(cellIndex);
+                if (cell == null) continue;
+                var cellLength = Encoding.UTF8.GetBytes(cell.ToString()!).Length;
+                if (cellLength > maxLength[cellIndex]) maxLength[cellIndex] = cellLength;
+            }
+        }
+
+        const int maxAllowedWidth = 255 * 256;
+        for (var i = 0; i < sheet.GetRow(0).LastCellNum; i++) // 使用表头行的单元格数量作为列数
+        {
+            var calculatedWidth = Math.Max(headerAverageWidth, maxLength[i] + 1) * 256;
+            sheet.SetColumnWidth(i, Math.Min(calculatedWidth, maxAllowedWidth)); // 限制列宽
+        }
+
+        MemoryStream stream = new();
+        workbook.Write(stream);
+        return new MemoryStream(stream.ToArray());
+    }
+}
---
+++ Infrastructure/Services/MinioService.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Extensions.Configuration;
+using Minio;
+
+namespace Infrastructure.Services;
+
+public class MinioService
+{
+    private readonly string _bucketName;
+    private readonly IMinioClient _minioClient;
+
+    public MinioService(IConfiguration configuration)
+    {
+        var minioEndpoint = configuration["Minio:Endpoint"] ?? "";
+        var minioAccessKey = configuration["Minio:AccessKey"] ?? "";
+        var minioSecretKey = configuration["Minio:SecretKey"] ?? "";
+        _bucketName = configuration["Minio:BucketName"] ?? "qms";
+        
+        _minioClient = new MinioClient()
+            .WithEndpoint(minioEndpoint)
+            .WithCredentials(minioAccessKey, minioSecretKey)
+            .Build();
+    }
+    
+    
+    
+    
+    public MemoryStream ConvertToMemoryStream(IFormFile formFile)
+    {
+        MemoryStream memoryStream = new();
+        formFile.CopyTo(memoryStream);
+        memoryStream.Seek(0, SeekOrigin.Begin); // 重置流的位置到开头，这样可以从头开始读取数据  
+        return memoryStream;
+    }
+    
+    public async Task<string> AsyncUploadFilesMinIO(FileUploadPara parm, string date)
+    {
+        // 检查是否存在桶
+        var bucketExists = await _minioClient.BucketExistsAsync(new BucketExistsArgs().WithBucket("mes2"))
+            .ConfigureAwait(false);
+        if (!bucketExists)
+            await _minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket("mes2")).ConfigureAwait(false);
+
+        // 验证数据是否为空
+        if (parm == null || parm.FileData == null) return "";
+
+        var folderName = parm.BusinessType?.ToLower() ?? "";
+        var uuid = _hashHelper.GetUUID();
+        var fileName = uuid + parm.FileSuffix;
+
+        // 重置流位置
+        parm.FileData.Position = 0;
+
+        // 获取文件类型
+        var provider = new FileExtensionContentTypeProvider();
+        if (!provider.TryGetContentType(fileName, out var mimeType)) mimeType = "application/octet-stream";
+
+        // 上传文件到 Minio
+        await _minioClient.PutObjectAsync(new PutObjectArgs()
+            .WithBucket(_bucketName)
+            .WithObject($"{folderName}/{fileName}") // 设置文件夹和文件名
+            .WithStreamData(parm.FileData)
+            .WithObjectSize(parm.FileData.Length)
+            .WithContentType(mimeType)).ConfigureAwait(false);
+
+        // 保存文件到files表
+        await AsyncSaveFileToTable(parm, uuid, date);
+
+        // 返回数据
+        return uuid;
+    }
+    
+    public async Task<FileStreamResult?> AsyncGetFilePreviewMinIO(string id)
+    {
+        // 获取文件 url 列表
+        var path = await AsyncGetFilePathMinIO(id);
+        if (string.IsNullOrEmpty(path.Path) || string.IsNullOrEmpty(path.FileName)) return null;
+        var filePath = path.Path;
+        var fileName = path.FileName;
+        var provider = new FileExtensionContentTypeProvider();
+        if (!provider.TryGetContentType(fileName, out var mimeType)) mimeType = "application/octet-stream";
+        // 创建内存流
+        MemoryStream memoryStream = new();
+        // 下载文件到 MemoryStream
+        var getObjectArgs = new GetObjectArgs()
+            .WithBucket(_bucketName)
+            .WithObject(filePath)
+            .WithCallbackStream(async stream => { await stream.CopyToAsync(memoryStream); });
+        var getObjectResult = await _minioClient.GetObjectAsync(getObjectArgs);
+
+        // 定位到 0
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        // 转成文件流
+        FileStreamResult fileStreamResult = new(memoryStream, mimeType)
+        {
+            FileDownloadName = fileName
+        };
+
+        return fileStreamResult;
+    }
+    
+    public async Task<string> AsyncDeleteFileMinIO(string id)
+    {
+        var path = await AsyncGetFilePathMinIO(id);
+        if (string.IsNullOrEmpty(path.Path) || string.IsNullOrEmpty(path.FileName)) return "";
+        var filePath = path.Path;
+        var removeObjectArgs = new RemoveObjectArgs()
+            .WithBucket(_bucketName)
+            .WithObject(filePath);
+        await _minioClient.RemoveObjectAsync(removeObjectArgs).ConfigureAwait(false);
+        ;
+        await AsyncDeleteFileToTableMinIO(id);
+        return "success";
+    }
+}
