99c962d37d90c808a6572ec69767c5da82816e5b
Mylinaa <hu0000604@outlook.com>
2025-07-05 21:18:40 +0800

    build(deps): 更新项目依赖版本
    - 更新 AutoMapper、Serilog、Swashbuckle 等多个包的版本
    - 修改数据库连接字符串中的服务器地址
    - 更新 Minio、NPOI、MailKit 等包的版本
    - 统一使用 Scrutor 6.1.0 版本- 更新 StackExchange.Redis 版本

--- Application/Mappings/AccountProfile.cs
+++ Application/Mappings/AccountProfile.cs
@@ -11,9 +11,7 @@ public class AccountProfile : Profile
         CreateMap<Account, AccountDto>()
             .ForMember(dest => dest.AccountType, opt => opt.MapFrom(src => src.AccountType.ToString()))
             .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive.ToString()))
-            .ForMember(dest => dest.LastLoginTime,
-                opt => opt.MapFrom(src =>
-                    src.LastLoginTime.HasValue ? src.LastLoginTime.Value.ToString("yyyy-MM-dd HH:mm:ss") : ""))
+            .ForMember(dest => dest.LastLoginTime, opt => opt.MapFrom(src => src.LastLoginTime.HasValue ? src.LastLoginTime.Value.ToString("yyyy-MM-dd HH:mm:ss") : ""))
             .ForMember(dest => dest.FailedLoginAttempts, opt => opt.MapFrom(src => src.FailedLoginAttempts.ToString()))
             .ForMember(dest => dest.IsLocked, opt => opt.MapFrom(src => src.IsLocked.ToString()));
     }
